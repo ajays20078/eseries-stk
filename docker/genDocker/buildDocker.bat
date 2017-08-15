e13d061229eb18230a31fd5eab4f0ab92b5fa5db
rkelderh <Richard.Kelderhouse@netapp.com>
2017-08-15 19:39:36 +0000

    Removed original docker image

--- docker/genDocker/buildDocker.bat
+++
@@ -1,3 +0,0 @@
-docker build -t myansiblessh .
-mkdir ansiblelink
-pause
--- docker/genDocker/runDocker.bat
+++
@@ -1,9 +0,0 @@
-SET var=%CD%
-SET driveSuffix=%var::\=/%
-
-SET fixSlashes=%driveSuffix:\=/%
-
-SET unixPath=/%fixSlashes%/ansiblelink
-
-docker run -v %unixPath%:/root/ansiblelink -ti myansiblessh
-PAUSE
