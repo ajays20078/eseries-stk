07101b13f050e7a42550e057c108108c1ad0906e
Wilder <dwilder@netapp.com>
2017-09-06 11:45:10 -0500

    Changes for non root users changed name of Splunk_indexer role to IO_hosts to enable reuse Added Changes for SLES 12 and Ubuntu 16 support Changed Label of DP to signify Hot and Cold Tiers Misc Readme Updates

--- roles/IO_hosts/files/linux_filesystems.sh
+++ roles/IO_hosts/files/linux_filesystems.sh
@@ -12,9 +12,6 @@ for mnt in $mnts; do
     fi
 done
 
-# Write IOMNT Section Footer
-echo "#IOMNT START#" >> /etc/fstab
-
 # find device mapper disks
 disks=`/sbin/multipath -l | egrep "NETAPP.*INF" | egrep "dm-[0-9]+" | awk '{print "/dev/mapper/"$1}'`
 
@@ -25,9 +22,6 @@ for disk in $disks; do
     mkdir /mnt/netapp-mnt-$tmp &> /dev/null
     echo -e "$disk\t/mnt/netapp-mnt-$tmp\t\t$fs_type\t$disk_mount_flags\t0 0" >> /etc/fstab
 done
-
-# Write IOMNT Section Footer
-echo "#IOMNT END#" >> /etc/fstab
 
 # Mount all targets
 mount -a &> /dev/null
